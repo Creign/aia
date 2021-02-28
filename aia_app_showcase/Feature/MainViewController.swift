783784fe80d9da4670c8a644ac7c65f6c014cdb1
Excell Nicolas <excell.nicolas@live.com>
2021-02-28 20:52:44 +0800

    added view controllers

---
+++ aia_app_showcase/Feature/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  aia_app_showcase
+//
+//  Created by Excell on 28/02/2021.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setViewControllers()
+    }
+}
+
+private extension MainViewController {
+    func setViewControllers() {
+        let firstViewController = FirstViewController()
+        firstViewController.tabBarItem = UITabBarItem(title: "First", image: nil, tag: 0)
+        
+        let secondViewController = SecondViewController()
+        secondViewController.tabBarItem = UITabBarItem(title: "Second", image: nil, tag: 1)
+        
+        let thirdViewController = ThirdViewController()
+        thirdViewController.tabBarItem = UITabBarItem(title: "Third", image: nil, tag: 2)
+        
+        viewControllers = [firstViewController, secondViewController, thirdViewController]
+    }
+}
---
+++ aia_app_showcase/Feature/ProjectViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectViewModel.swift
+//  aia_app_showcase
+//
+//  Created by Excell on 28/02/2021.
+//
+
+import Foundation
+
+class ProjectViewModel {
+    
+    
+    
+}
