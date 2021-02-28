19a93aa484a8077375b8d580ae79c5b2ee4b28cd
Excell Nicolas <excell.nicolas@live.com>
2021-02-28 20:09:04 +0800

    init

---
+++ aia_app_showcase/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  aia_app_showcase
+//
+//  Created by Excell on 28/02/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ aia_app_showcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aia_app_showcase
+//
+//  Created by Excell on 28/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
